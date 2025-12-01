f526876c0a10f26b8c9e06f100e0d7ac31c816e4
Pablo Gonzalez Segarra <pablogscraft@gmail.com>
2025-12-01 12:57:28 +0100

    commit

--- ej8.cu
+++ ej8.cu
@@ -6,9 +6,11 @@
 #include <cuda_runtime.h>
 
 __global__ void vector_pro(float *out, float *a, float *b, int n) {
-    int i = blockIdx.x * blockDim.x + threadIdx.x;
-    if (i < n) {
-        out[i] = a[i] + b[i];
+    for(int i = 0; i < n; i++){
+        out[i] = 0;
+        for (int j= 0; j<n; j++){
+            out[i] += a[i]*b[j];
+        }
     }
 }
 
@@ -28,6 +30,22 @@ int main(int argc, char **argv){
 
     float *a_cuda = NULL, *b_cuda = NULL, *out_cuda = NULL;
 
+    
+    // Allocate device memory
+    cudaMalloc((void**)&a_cuda, sizeof(float) * N);
+    cudaMalloc((void**)&b_cuda, sizeof(float) * N);
+    cudaMalloc((void**)&out_cuda, sizeof(float) * N);
+    
+    // Copy inputs to device
+    cudaMemcpy(a_cuda, a, sizeof(float) * N, cudaMemcpyHostToDevice);
+    cudaMemcpy(b_cuda, b, sizeof(float) * N, cudaMemcpyHostToDevice);
+    
+    // threadsPerBlock is taken from argv[1] (assume valid integer provided)
+    int threadsPerBlock = atoi(argv[1]);
+    int blocks = (N + threadsPerBlock - 1) / threadsPerBlock;
+    // Print configuration
+    printf("Using %d blocks of %d threads\n", blocks, threadsPerBlock);
+
     // Create events for timing
     cudaEvent_t start, end;
     cudaEventCreate(&start);
@@ -35,25 +53,8 @@ int main(int argc, char **argv){
 
     // Start timing
     cudaEventRecord(start);
-
-    // Allocate device memory
-    cudaMalloc((void**)&a_cuda, sizeof(float) * N);
-    cudaMalloc((void**)&b_cuda, sizeof(float) * N);
-    cudaMalloc((void**)&out_cuda, sizeof(float) * N);
-
-    // Copy inputs to device
-    cudaMemcpy(a_cuda, a, sizeof(float) * N, cudaMemcpyHostToDevice);
-    cudaMemcpy(b_cuda, b, sizeof(float) * N, cudaMemcpyHostToDevice);
-
     // Main function
-    // threadsPerBlock is taken from argv[1] (assume valid integer provided)
-    int threadsPerBlock = atoi(argv[1]);
-    int blocks = (N + threadsPerBlock - 1) / threadsPerBlock;
-    // Print configuration
-    printf("Using %d blocks of %d threads\n", blocks, threadsPerBlock);
-    vector_add<<<blocks, threadsPerBlock>>>(out_cuda, a_cuda, b_cuda, N);
-
-    cudaMemcpy(out, out_cuda, sizeof(float) * N, cudaMemcpyDeviceToHost);
+    vector_pro<<<blocks, threadsPerBlock>>>(out_cuda, a_cuda, b_cuda, N);
 
     // End timing
     cudaEventRecord(end);
@@ -61,17 +62,19 @@ int main(int argc, char **argv){
     float milliseconds = 0;
     cudaEventElapsedTime(&milliseconds, start, end);
     printf("Time elapsed: %f ms\n", milliseconds);
+    
+    cudaMemcpy(out, out_cuda, sizeof(float) * N, cudaMemcpyDeviceToHost);
 
     // Verify result
-    const float MAX_ERR = 1e-6f;
-    for(int i = 0; i < N; i++){
-        if(fabsf(out[i] - 3.0f) > MAX_ERR){
-            printf("Error at index %d: %f (diff=%f)\n", i, out[i], fabsf(out[i] - 3.0f));
-            return -1;
-        }
-    }
+    // const float MAX_ERR = 1e-6f;
+    // for(int i = 0; i < N; i++){
+    //     if(fabsf(out[i] - 3.0f) > MAX_ERR){
+    //         printf("Error at index %d: %f (diff=%f)\n", i, out[i], fabsf(out[i] - 3.0f));
+    //         return -1;
+    //     }
+    // }
 
-    printf("Success! All values are correct.\n");
+    printf("Success!\n");
 
     // Free memory
     cudaFree(a_cuda);
